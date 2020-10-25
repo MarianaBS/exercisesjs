b747413e732b98adf37e94d5282578f0728ee8a2
Mariana Sanchez <marianabsanchez@hotmail.com>
2020-10-24 23:35:11 -0300

    Ejercicio 6

--- exercises.js
+++ exercises.js
@@ -216,11 +216,92 @@ for(var i=0;i<10;i++){
 }
 console.log(arreglo);
 
-/*Funciones
-Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
-A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
-Crear una función validate integer que reciba un número como parámetro y verdadero si es un número entero.
-A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
-Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
-*/
 
+//Funciones
+
+/*a-Crear una función suma que reciba dos valores numéricos y retorne el resultado. 
+Ejecutar la función y guardar el resultado en una variable, 
+mostrando el valor de dicha variable en la consola del navegador.*/
+
+function sumar(num1,num2){
+    return num1+num2;
+}
+var resultado=sumar(10,27);
+console.log(resultado);
+
+
+/*b-A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, 
+mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.*/
+
+function sumar(num1,num2){
+    if(typeof(num1)!='number' || typeof(num2)!='number'){
+        alert('Uno de los parámetros no es un número');
+        return NaN;
+    }else{
+        return num1+num2;
+    }
+}
+var resultado=sumar(10,27);
+console.log(resultado);
+var resultado=sumar('mariana',27);
+console.log(resultado);
+
+/*c-Crear una función validate integer que reciba un número como parámetro 
+y verdadero si es un número entero.*/
+
+function validate_integer(numero){
+    if (numero % 1 == 0) {
+        return true;
+     } else {
+        return false;
+     }
+}
+console.log(validate_integer(10.5));
+console.log(validate_integer(21));
+
+/*d-A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. 
+En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).*/
+
+function sumar(num1,num2){
+    if(typeof(num1)!='number' || typeof(num2)!='number'){
+        alert('Uno de los parámetros no es un número');
+        return NaN;
+    }else{
+        if (!validate_integer(num1)){
+            alert('El primer número ingresado no es entero. Se ha redondeado al más próximo');
+            num1=Math.round(num1);
+        }
+        if(!validate_integer(num2)){
+            alert('El segundo número ingresado no es entero. Se ha redondeado al más próximo');
+            num2=Math.round(num2);
+        }
+        return num1+num2;
+    }  
+}
+console.log(sumar(2.5,3.3));
+console.log(sumar(2.5,8));
+console.log(sumar(11,3.3));
+console.log(sumar(22,28));
+
+
+/*E-Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma
+ probando que todo siga funcionando igual.*/
+
+ function esNumero(n){
+     if(typeof(n)!='number'){
+        return false;
+     }else{
+         return true;
+     }
+ }
+ function sumar(n1,n2){
+     if(!esNumero(n1) || !esNumero(n2)){
+        alert('Uno de los parámetros no es un número');
+        return NaN;
+     }
+     else{
+         return n1+n2;
+     }
+ }
+console.log(sumar(4,9));
+console.log(sumar('m','a'));
